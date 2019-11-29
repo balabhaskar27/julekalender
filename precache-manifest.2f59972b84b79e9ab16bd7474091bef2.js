809b8aeda5d63f0b194baba3cb4c855d7a95805a
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-11-29 09:02:25 +0100

    Updates

---
+++ precache-manifest.2f59972b84b79e9ab16bd7474091bef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ea2fa32711ed428da7087800da08a0",
+    "url": "/julekalender/index.html"
+  },
+  {
+    "revision": "0fc7763dbe7783ffe9b6",
+    "url": "/julekalender/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "18135b4b5b8778398cb3",
+    "url": "/julekalender/static/css/main.3d430106.chunk.css"
+  },
+  {
+    "revision": "0fc7763dbe7783ffe9b6",
+    "url": "/julekalender/static/js/2.3f6b5a28.chunk.js"
+  },
+  {
+    "revision": "18135b4b5b8778398cb3",
+    "url": "/julekalender/static/js/main.33a0fe67.chunk.js"
+  },
+  {
+    "revision": "3cc606fe6ed1d83ca8ec",
+    "url": "/julekalender/static/js/runtime-main.9b8ea490.js"
+  },
+  {
+    "revision": "c4551d30e17389cf21ff169e1020e18f",
+    "url": "/julekalender/static/media/logo-offer.c4551d30.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40411a46dbc7d91e6834e3c27feb7be2.js"
+  "/julekalender/precache-manifest.2f59972b84b79e9ab16bd7474091bef2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/julekalender/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
